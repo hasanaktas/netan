cbff78409417c7c2d129146365abdcbaf7f11dee
mpolat35 <68499598+mpolat35@users.noreply.github.com>
2020-07-22 23:58:49 +0300

    deneme

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div className='container'>
       <Head>
-        <title>Create Next App</title>
+        <title>Deneme
+        </title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header />
