dd42aa766610a5ec6dfa46c0704dd73a037fb6b4
mpolat35 <68499598+mpolat35@users.noreply.github.com>
2020-07-23 10:45:19 +0300

    Update index.js
    aaaa

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className='container'>
       <Head>
-        <title>Deneme
+        <title>Deneme2
         </title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
